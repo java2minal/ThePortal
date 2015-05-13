9bd13a259561d1c5f18346b7d1235c51b9885737
java2minal <minal_saeed@hotmail.com>
2015-05-13 18:59:45 +0800

    onclick sound push

--- app/src/main/java/com/luct/minal/theportal/MainActivity.java
+++ app/src/main/java/com/luct/minal/theportal/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
 import android.widget.ImageButton;
 
 import com.luct.minal.theportal.Java2.Effects;
